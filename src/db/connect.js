3b6c2100e259a640419d25f0d42f729169d469b4
sachins0412 <saxenas964@gmail.com>
2022-11-06 21:01:27 +0530

    change to env var

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = "mongodb://127.0.0.1:27017/email-scheduler";
+const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/email-scheduler";
 
 const connect = () => {
   mongoose
